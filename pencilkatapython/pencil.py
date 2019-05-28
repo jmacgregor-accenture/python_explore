01a6d3d7a98f053ae80bfad1c7c9af16e1753e80
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-28 11:37:04 -0400

    Pencil gets created with starting durability

--- pencilkatapython/pencil.py
+++ pencilkatapython/pencil.py
@@ -1,2 +1,5 @@
 class Pencil:
     durability = 0
+
+    def __init__(self, startDurability):
+        self.durability = startDurability
--- tests/test_pencil.py
+++ tests/test_pencil.py
@@ -1,6 +1,12 @@
 from pencilkatapython.pencil import Pencil
 
 def test_pencilExists():
-    pencil = Pencil()
+    pencil = Pencil(0)
 
     assert pencil.durability == 0
+
+def test_canBeSetWithDurability():
+    testDurability = 25
+    pencil = Pencil(testDurability)
+
+    assert pencil.durability == 25
